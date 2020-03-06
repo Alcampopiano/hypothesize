d66cf89d45116db4552219f8b5443ea3f9090c60
Allan Campopiano <campopianoal@hcdsb.org>
2020-03-06 16:05:27 -0500

    updated dev version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(path, encoding="utf-8"):
 
 setup(
     name='hypothesize',
-    version='0.1dev2',
+    version='0.1dev3',
     description='A Python package for comparing groups and measuring associations using robust statistics.',
     author='Allan Campopiano',
     author_email="campopianoa@hcdsb.org",
