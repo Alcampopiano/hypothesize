d323b875505c35b612f558f04d42f61dac97a981
Allan Campopiano <campopianoal@hcdsb.org>
2020-03-13 14:45:49 -0400

    added linconb and supporting functions for one-way robust comparisons using linear contrasts

--- hypothesize/compare_groups_with_single_factor/_compare_groups_with_single_factor.py
+++ hypothesize/compare_groups_with_single_factor/_compare_groups_with_single_factor.py
@@ -1,8 +1,9 @@
-__all__ = ["yuenbt"]
+__all__ = ["yuenbt", "linconb"]
 
 import numpy as np
+import pandas as pd
 from scipy.stats import trim_mean
-from hypothesize.utilities import winvar, trimse
+from hypothesize.utilities import trimse, lincon, trimparts, trimpartt
 
 def yuenbt(x, y, tr=.2, alpha=.05, nboot=599, seed=False):
 
@@ -58,6 +59,99 @@ def yuenbt(x, y, tr=.2, alpha=.05, nboot=599, seed=False):
 
 
     return ci, test_stat, p_value, est_x, est_y, est_dif
+
+def linconb(x, con, tr, alpha, nboot, seed=False):
+
+    """
+    Compute a 1-alpha confidence interval for a set of d linear contrasts
+    involving trimmed means using the bootstrap-t bootstrap method.
+    Independent groups are assumed.
+
+    CIs are adjusted to control FWE (p values are not adjusted)
+
+    x[1] contains the data for the first group, x[2] the data
+    for the second group, etc. len(x)= the number of groups = J
+
+    Missing values are automatically removed.
+
+    con is a J by d matrix containing the contrast coefficents of interest.
+    If unspecified, all pairwise comparisons are performed.
+    For example, con[:,0]=[1,1,-1,-1,0,0] and con[:,1]=[1,-1,0,0,1,-1]
+    will test two contrasts: (1) the sum of the first two trimmed means is
+    equal to the sum of the second two, and (2) the difference between
+    the first two is equal to the difference between the trimmed means of
+    groups 5 and 6.
+
+    The default number of bootstrap samples is nboot=599
+
+    :param x: 1 x number of groups array. x[0] contains the data for the first group, and so on
+    :param tr: amount of trimming
+    :param con: contrast matrix (see con1way)
+    :param alpha: alpha level
+    :param nboot: number of bootstrap samples
+    :param seed: random seed for reproducibility
+    :return: n for each group, psihat, test statistic, critical value, contrast matrix
+    """
+
+    J = len(x)
+    x = np.asarray([j[~np.isnan(j)] for j in x])
+    #Jm = J - 1
+    #d = (J ** 2 - J) / 2
+
+    if con.shape[0] != len(x):
+      raise Exception("The number of groups does not match the number of contrast coefficients.")
+
+    bvec = np.zeros([nboot, J, 2])
+
+    if seed:
+        np.random.seed(seed)
+
+    nsam = [len(xi) for xi in x]
+    for j in range(J):
+
+        xcen = x[j] - trim_mean(x[j], tr)
+        data = np.random.choice(xcen, size=(nboot, len(x[j])))
+
+        for i, row in enumerate(data):
+            bvec[i,j,:]=trimparts(row, tr)
+
+    m1 = bvec[:,:,0].T
+    m2 = bvec[:,:, 1].T
+    boot = np.zeros([con.shape[1], nboot])
+    for d in range(con.shape[1]):
+        top = np.asarray([trimpartt(row, con[:,d]) for row in m1.T])
+        consq = con[:, d] ** 2
+        bot = np.asarray([trimpartt(row,consq) for row in m2.T])
+        boot[d,:] = np.abs(top) / np.sqrt(bot)
+
+    testb=np.asarray([max(row) for row in boot.T])
+    ic = int(np.floor((1 - alpha) * nboot) -1) # one less than R
+    testb = np.sort(testb)
+    psihat = np.zeros([con.shape[1], 4])
+    test = np.zeros([con.shape[1], 4])
+
+    for d in range(con.shape[1]):
+        test[d, 0] = d
+        psihat[d, 0] = d
+        #testit = lincon(x, con[:, d], tr, alpha) #np.array([con[:,0]]).T
+        testit = lincon(x, np.array([con[:,d]]).T, tr, alpha) # column slice of contrast matrix
+        #print(testit)
+        test[d, 1]=testit['test'][0, 1]
+        pval = np.mean((abs(testit['test'][0, 1]) < boot[d,:]))
+        test[d, 3] = pval
+        psihat[d, 2] = testit['psihat'][0, 1] - testb[ic] * testit['test'][0, 3]
+        psihat[d, 3] = testit['psihat'][0, 1] + testb[ic] * testit['test'][0, 3]
+        psihat[d, 1] = testit['psihat'][0, 1]
+        test[d, 2] = testit['test'][0, 3]
+
+
+    psihat_col_names=['contrast_index', 'psihat', 'ci_low', 'ci_up']
+    test_col_names = ['contrast_index', 'test', 'se', 'p_value']
+
+    psihat = pd.DataFrame(psihat, columns=psihat_col_names)
+    test=pd.DataFrame(test, columns=test_col_names)
+
+    return {'n': nsam, 'psihat': psihat,  'test': test, 'crit': testb[ic], 'con': con}
 
 
 
