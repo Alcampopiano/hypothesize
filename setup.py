8f1a939efd2e329f8073bf3a7ebb16b811271d75
alcampopiano <alcampopiano@gmail.com>
2020-03-23 17:31:30 -0400

    added random data code to colab so it is useable straight away

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(path, encoding="utf-8"):
 
 setup(
     name='hypothesize',
-    version='0.1.dev16',
+    version='0.1.dev17',
     description='A Python package for comparing groups and measuring associations using robust statistics.',
     author='Allan Campopiano',
     author_email="campopianoa@hcdsb.org",
