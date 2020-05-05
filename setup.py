2b9ed4809e57746b0edd91e9c947beceb888f1e2
alcampopiano <alcampopiano@gmail.com>
2020-05-05 14:13:24 -0400

    enter beta stage

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(path, encoding="utf-8"):
 
 setup(
     name='hypothesize',
-    version='0.1.b1',
+    version='0.1b1',
     description='A Python package for comparing groups and measuring associations using robust statistics.',
     author='Allan Campopiano',
     author_email="campopianoa@hcdsb.org",
