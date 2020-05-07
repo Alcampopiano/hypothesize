d90f23af05f442e93e5473e932afaec29d30b5ad
alcampopiano <alcampopiano@gmail.com>
2020-05-07 19:58:28 -0400

    testing path issues

--- hypothesize/tests/test_funcs.py
+++ hypothesize/tests/test_funcs.py
@@ -91,6 +91,7 @@ def check_dict_items_equality(expected_results, actual_results):
     return actual_truth
 
 def test_l2drmci():
+    os.chdir('tests')
     print('HHHEEERRREEE', os.getcwd())
 
     np.random.seed(42)
