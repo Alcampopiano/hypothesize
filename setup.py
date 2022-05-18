e76fc512bcce2968ca18019da31ca06663fb0be2
Allan Campopiano <alcampopiano@gmail.com>
2022-05-18 17:19:23 -0400

    updated version and tests

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(path, encoding="utf-8"):
 
 setup(
     name='hypothesize',
-    version='1.1.1',
+    version='1.2.0',
     description='A Python package for comparing groups and measuring associations using robust statistics.',
     author='Allan Campopiano',
     author_email="campopianoa@hcdsb.org",
